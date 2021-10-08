721daaca34962610eafd9e6e363f89159f33e432
tiger0ym <0t.a40622@gmail.com>
2021-10-08 22:45:34 +0900

    add: support for smartphone

--- js/sketch.js
+++ js/sketch.js
@@ -33,28 +33,15 @@ function doubleClicked() {
 }
 */
 
-function setup() {
-  createCanvas(displayWidth, displayHeight);
-  background(255);
-
-  strokeWeight(10)
-  stroke(0);
+function setup(){
+  createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-  //スマホ用
-  ptouchX = touches[0].x;
-  ptouchY = touches[0].y;
-}
+function draw(){
+  background(0);
 
-//スマホ用
-function touchMoved() {
-  stroke('#66CDAA');
-  line(touches[0].x, touches[0].y, ptouchX, ptouchY);
-  return false;
-}
-
-//背景リセット
-function doubleClicked() {
-  background(255);
+  if (mouseIsPressed) {
+    fill(255);
+    ellipse(mouseX, mouseY, 50, 50);
+  }
 }
