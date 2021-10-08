0d92547270b2713a5b018f275d00187e3877a46c
tiger0ym <0t.a40622@gmail.com>
2021-10-08 22:35:17 +0900

    update: line color

--- js/sketch.js
+++ js/sketch.js
@@ -32,20 +32,29 @@ function doubleClicked() {
   background(255);
 }
 */
-// TouchDraw
+
 function setup() {
-  devicePixelScaling(false);
-  createCanvas(windowWidth, windowHeight);
-  stroke(0); // 黒く塗りつぶす
-  strokeWeight(20); // 幅を20に
- 	background(200);
+  createCanvas(displayWidth, displayHeight);
+  background(255);
+
+  strokeWeight(10)
+  stroke(0);
 }
 
 function draw() {
-	
+  //スマホ用
+  ptouchX = touches[0].x;
+  ptouchY = touches[0].y;
 }
 
+//スマホ用
 function touchMoved() {
-	line(touchX, touchY, ptouchX, ptouchY);
+  stroke('#66CDAA');
+  line(touches[0].x, touches[0].y, ptouchX, ptouchY);
   return false;
+}
+
+//背景リセット
+function doubleClicked() {
+  background(255);
 }
