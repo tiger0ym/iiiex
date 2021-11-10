0da13f74eb24536c9ea8b4f18fe85592766f6631
tiger0ym <0t.a40622@gmail.com>
2021-11-10 13:46:43 +0900

    delete: message when embedded

--- js/index.js
+++ js/index.js
@@ -3,6 +3,4 @@ if (window == window.parent) {
     "message"
   ).innerHTML = `スマホの方は<a href="./mobile.html">こちら</a>`;
 } else {
-  document.getElementById("message").innerHTML =
-    "右下のボタンから全画面にしてください";
 }
