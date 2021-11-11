ee0cf4e94c6c65cdb7494597beffbbc0f960afe7
tiger0ym <0t.a40622@gmail.com>
2021-11-11 11:30:19 +0900

    add: change layout accroding to device 2

--- js/gameend.js
+++ js/gameend.js
@@ -1,8 +1,6 @@
 var replayDiv = document.getElementsByClassName("replay");
 var resultDiv = document.getElementsByClassName("result");
 var aaa = document.getElementById("wrapper");
-console.log(replayDiv[0]);
-console.log(aaa);
 if (window == window.parent) {
   replayDiv[0].className = "btn replay not-embedded";
   resultDiv[0].className = "btn result not-embedded";
