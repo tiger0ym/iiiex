c446e956407c6c4143e15e0c9c9273e98a3e28b8
tiger0ym <0t.a40622@gmail.com>
2021-11-14 19:04:20 +0900

    add: change color

--- js/sketch.js
+++ js/sketch.js
@@ -234,14 +234,17 @@ function draw() {
     if (isSmartPhone()) {
       if (window == window.parent) {
         textSize(100);
+        fill(255, 0, 0);
+        strokeWeight(5);
+        textAlign(CENTER);
+        text("タップしてスタート", windowWidth / 2, windowHeight / 2);
       } else {
         textSize(20);
+        fill(0, 0, 0);
+        strokeWeight(5);
+        textAlign(CENTER);
+        text("タップしてスタート", windowWidth / 2, windowHeight / 2);
       }
-      fill(0, 0, 0);
-      strokeWeight(5);
-
-      textAlign(CENTER);
-      text("タップしてスタート", windowWidth / 2, windowHeight / 2);
     } else {
       fill(0, 0, 0);
       strokeWeight(3);
