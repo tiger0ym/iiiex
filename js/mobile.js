fb84788892dc85d85bebae6b6b81c709ebe0605d
tiger0ym <0t.a40622@gmail.com>
2021-11-16 13:54:36 +0900

    add: change toppage styling on smartphone when not embedded

--- js/mobile.js
+++ js/mobile.js
@@ -1,18 +1,27 @@
-var introDiv = document.getElementsByClassName("intro");
 var gamestartDiv = document.getElementsByClassName("gamestart");
+var secTitleDiv = document.getElementsByClassName("sec-title");
+var secContainWrapperDiv = document.getElementsByClassName(
+  "sec-contain-wrapper"
+);
 if (window == window.parent) {
-  if (introDiv.length != 0) {
-    introDiv[0].className = "btn intro not-embedded";
-  }
   if (gamestartDiv.length != 0) {
     gamestartDiv[0].className = "btn gamestart not-embedded";
   }
+  for (let i = 0; i < 2; i++) {
+    secTitleDiv[i].className = "sec-title not-embedded";
+  }
+  for (let i = 0; i < 2; i++) {
+    secContainWrapperDiv[i].className = "sec-contain-wrapper not-embedded";
+  }
 } else {
   document.getElementById("message").innerHTML = `全画面表示にしてください`;
-  if (introDiv.length != 0) {
-    introDiv[0].className = "btn intro embedded";
-  }
   if (gamestartDiv.length != 0) {
     gamestartDiv[0].className = "btn gamestart embedded";
+  }
+  for (let i = 0; i < 2; i++) {
+    secTitleDiv[i].className = "sec-title embedded";
+  }
+  for (let i = 0; i < 2; i++) {
+    secContainWrapperDiv[i].className = "sec-contain-wrapper embedded";
   }
 }
