7107787559448b8b002544323a5f694f7c7f15a1
tiger0ym <0t.a40622@gmail.com>
2021-11-17 12:52:21 +0900

    change: text color

--- js/sketch.js
+++ js/sketch.js
@@ -233,7 +233,7 @@ function draw() {
       }
       fill(0, 0, 0);
       strokeWeight(10);
-      stroke(#26dd26);
+      stroke(38, 221, 38);
       textAlign(CENTER);
       text("タップしてスタート", windowWidth / 2, windowHeight / 2);
     } else {
