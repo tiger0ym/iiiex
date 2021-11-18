3e2b18fb2236bff5d47185e75ea5abb9d498e0c0
tiger0ym <0t.a40622@gmail.com>
2021-11-18 15:34:47 +0900

    add: new jsondata and image

--- js/sketch.js
+++ js/sketch.js
@@ -82,7 +82,7 @@ let userAgent;
 
 function preload() {
   //data
-  jsonData = loadJSON("./data/7days_interval300_3.json");
+  jsonData = loadJSON("./data/7days_interval300.json");
   //image
   //bgImage = loadImage("./image/kisho_demo.JPG");
   bgSuimin = loadImage("./image/suimin.JPG");
