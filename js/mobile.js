d8daaffb6788a2129a40f3ff9eab34f7d73d66e7
tiger0ym <0t.a40622@gmail.com>
2021-11-19 06:23:53 +0900

    fix: mobile2 etc

--- js/mobile.js
+++ js/mobile.js
@@ -3,25 +3,34 @@ var secTitleDiv = document.getElementsByClassName("sec-title");
 var secContainWrapperDiv = document.getElementsByClassName(
   "sec-contain-wrapper"
 );
+
 if (window === window.parent) {
   if (gamestartDiv.length != 0) {
     gamestartDiv[0].className = "btn gamestart not-embedded";
   }
+  secTitleDiv[0].className = "sec-title not-embedded";
+  secContainWrapperDiv[0].className = "sec-contain-wrapper not-embedded";
+  /*
   for (let i = 0; i < 2; i++) {
     secTitleDiv[i].className = "sec-title not-embedded";
   }
   for (let i = 0; i < 2; i++) {
     secContainWrapperDiv[i].className = "sec-contain-wrapper not-embedded";
   }
+  */
 } else {
   document.getElementById("message").innerHTML = `全画面表示にしてください`;
   if (gamestartDiv.length != 0) {
     gamestartDiv[0].className = "btn gamestart embedded";
   }
+  secTitleDiv[0].className = "sec-title embedded";
+  secContainWrapperDiv[0].className = "sec-contain-wrapper embedded";
+  /*
   for (let i = 0; i < 2; i++) {
     secTitleDiv[i].className = "sec-title embedded";
   }
   for (let i = 0; i < 2; i++) {
     secContainWrapperDiv[i].className = "sec-contain-wrapper embedded";
   }
+  */
 }
