86dedbb5b352e5fd1c993ce06354b019a4b7f6de
tiger0ym <0t.a40622@gmail.com>
2021-11-18 11:08:47 +0900

    add: graph on gameend page

--- js/gameend.js
+++ js/gameend.js
@@ -8,3 +8,23 @@ if (window == window.parent) {
   replayDiv[0].className = "btn replay embedded";
   resultDiv[0].className = "btn result embedded";
 }
+
+let resultData = JSON.parse(sessionStorage.getItem("resultJSON"));
+let achieveRateDen = 0; //分母
+let achieveRateNum = 0; //分子
+for (let i = 0; i < 4; i++) {
+  achieveRateDen += resultData.great[i] + resultData.miss[i];
+  achieveRateNum += resultData.great[i];
+}
+let achieveRate = Math.floor((achieveRateNum * 100) / achieveRateDen);
+var parent = document.getElementById("parent");
+const resultGraphDiv = document.createElement("div");
+resultGraphDiv.className = "result-graph";
+resultGraphDiv.innerHTML = achieveRate + "%";
+resultGraphDiv.style.backgroundImage =
+  `conic-gradient(#26dd26 0% ` +
+  achieveRate +
+  `%, rgba(0, 0, 0, 0) ` +
+  achieveRate +
+  `% 100%)`;
+parent.appendChild(resultGraphDiv);
