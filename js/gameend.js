d37a68ea86be7477aa6dd63307e2d0b9a68354e0
tiger0ym <0t.a40622@gmail.com>
2021-11-18 15:11:06 +0900

    add: pie chart animation

--- js/gameend.js
+++ js/gameend.js
@@ -8,23 +8,25 @@ if (window == window.parent) {
   replayDiv[0].className = "btn replay embedded";
   resultDiv[0].className = "btn result embedded";
 }
-
-let resultData = JSON.parse(sessionStorage.getItem("resultJSON"));
-let achieveRateDen = 0; //分母
-let achieveRateNum = 0; //分子
+/*
+//円グラフ(動かないver)
+let resultData2 = JSON.parse(sessionStorage.getItem("resultJSON"));
+let achieveRateDen2 = 0; //分母
+let achieveRateNum2 = 0; //分子
 for (let i = 0; i < 4; i++) {
-  achieveRateDen += resultData.great[i] + resultData.miss[i];
-  achieveRateNum += resultData.great[i];
+  achieveRateDen2 += resultData2.great[i] + resultData2.miss[i];
+  achieveRateNum2 += resultData2.great[i];
 }
-let achieveRate = Math.floor((achieveRateNum * 100) / achieveRateDen);
+let achieveRate2 = Math.floor((achieveRateNum2 * 100) / achieveRateDen2);
 var parent = document.getElementById("parent");
 const resultGraphDiv = document.createElement("div");
 resultGraphDiv.className = "result-graph";
-resultGraphDiv.innerHTML = achieveRate + "%";
+resultGraphDiv.innerHTML = achieveRate2 + "%";
 resultGraphDiv.style.backgroundImage =
   `conic-gradient(#26dd26 0% ` +
-  achieveRate +
+  achieveRate2 +
   `%, rgba(0, 0, 0, 0) ` +
-  achieveRate +
+  achieveRate2 +
   `% 100%)`;
 parent.appendChild(resultGraphDiv);
+*/
