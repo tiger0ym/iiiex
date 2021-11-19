14fe74a985ffa1fbe0707f5426f5a93190af3810
morningnaomin <92291045+morningnaomin@users.noreply.github.com>
2021-11-19 15:03:49 +0900

    change color

--- js/sketch.js
+++ js/sketch.js
@@ -270,13 +270,13 @@ function draw() {
       } else {
         textSize(30);
       }
-      fill(0, 0, 0);
+      fill(#FF40FF);
       strokeWeight(10);
-      stroke(38, 221, 38);
+      stroke(255,255,255);
       textAlign(CENTER);
       text("タップしてスタート", windowWidth / 2, windowHeight / 2);
     } else {
-      fill(0, 0, 0);
+      fill(#FF40FF);
       strokeWeight(5);
       stroke(255, 255, 255);
       textSize(40);
