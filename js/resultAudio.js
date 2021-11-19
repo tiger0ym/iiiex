8548d7125d3d1861fa12f234f277d86dc6d8c37f
tiger0ym <0t.a40622@gmail.com>
2021-11-19 12:33:09 +0900

    文字を変えました

--- js/resultAudio.js
+++ js/resultAudio.js
@@ -4,7 +4,7 @@ const btn = document.getElementById("BGMbtn"); // <button>
 btn.addEventListener("click", () => {
   // pausedがtrue=>停止, false=>再生中
   if (!BGM.paused) {
-    btn.innerHTML = "再生"; // 「再生ボタン」に切り替え
+    btn.innerHTML = "▶再生"; // 「再生ボタン」に切り替え
     BGM.pause();
     btn.style.color = "#ffffff";
     btn.style.backgroundColor = "#e4823d";
